35be93b3f85cd425fbc114a0ca9bcbec63c21920
Rishabh gupta <guptarishabh79@gmail.com>
2024-11-26 11:47:46 +0530

    make pageresponsives (wishlist

--- src/app/wishlist/components/WishlistCard.tsx
+++ src/app/wishlist/components/WishlistCard.tsx
@@ -4,8 +4,8 @@ import { RxCross1 } from "react-icons/rx";
 
 type Product = {
   ID: string;
-  date: string;
   EndPrice: string;
+  createdAt: string;
   Description: string;
   ListingImage: string;
 };
@@ -21,36 +21,42 @@ const WishlistCard: React.FC<WishlistCardProps> = ({
   onAddToCart,
   handleRemove,
 }) => {
-  const newdate: any = new Date();
+  const newdate: any = new Date(product?.createdAt);
   return (
-    <div className="flex items-center justify-between border hover:bg-gray-100 cursor-pointer transition-all duration-200 ease-linear">
-      <div className="flex justify-center items-center">
+    <div className="flex flex-col md:flex-row items-center justify-between border hover:bg-gray-100 cursor-pointer transition-all duration-200 ease-linear">
+      <div className="flex justify-center items-start md:items-center">
         <span
           onClick={() => handleRemove(product?.ID)}
-          className="flex justify-center hover:bg-gray-200 items-center h-full p-4 transition-all duration-200 ease-linear"
+          className="flex justify-center hover:bg-gray-200 items-center my-auto h-full p-3 md:p-4 transition-all duration-200 ease-linear"
         >
           <RxCross1 />{" "}
         </span>
-        <div className="flex items-start border-l border-gray-300 gap-4 p-4">
+        <div className="flex items-start border-l border-gray-300 gap-4 p-3 md:p-4">
           <Image
             width={200}
             height={200}
             alt={product.Description}
             src={product.ListingImage}
-            className="w-20 h-20 object-cover rounded-md"
+            className="max-w-20 max-h-20 aspect-square object-cover rounded-md"
           />
         </div>
-        <div>
-          <h2 className="text-xl font-bold text-gray-800">
+        <div className="pt-2 md:pt-0">
+          <h2 className="text-lg md:text-xl font-bold text-gray-800">
             {product.Description}
           </h2>
           <p className="text-gray-500 font-medium">{product.EndPrice}$</p>
           <p className="text-gray-400 text-sm">{newdate.toString()}</p>
+          <button
+            onClick={() => onAddToCart(product)}
+            className="bg-yellow-400 md:hidden my-2 text-black font-semibold px-6 text-sm mr-4 py-2 rounded-full hover:bg-black hover:text-white transition-all duration-150 ease-in-out"
+          >
+            ADD TO CART
+          </button>
         </div>
       </div>
       <button
         onClick={() => onAddToCart(product)}
-        className="bg-yellow-400 text-black font-semibold px-6 text-sm mr-4 py-2 rounded-full hover:bg-black hover:text-white transition-all duration-150 ease-in-out"
+        className="bg-yellow-400 hidden md:block text-black font-semibold px-6 text-sm mr-4 py-2 rounded-full hover:bg-black hover:text-white transition-all duration-150 ease-in-out"
       >
         ADD TO CART
       </button>
--- src/app/wishlist/page.tsx
+++ src/app/wishlist/page.tsx
@@ -22,16 +22,19 @@ export default function Page() {
   };
   return (
     <>
-      <div className="max-w-9xl mx-auto p-4 md:p-6 lg:p-10">
+      <div className="max-w-9xl min-h-screen mx-auto p-4 md:p-6 lg:p-10">
         <div className="flex justify-between items-center">
           <h1
-            className={`uppercase text-7xl flex items-center font-black ${bigShoulders.className}`}
+            className={`uppercase text-3xl md:text-5xl lg:text-7xl flex items-center font-black ${bigShoulders.className}`}
           >
             Wishlist{" "}
-            <span className="text-4xl"> ({wishlistUpdated.length})</span>
+            <span className="text-lg md:text-2xl lg:text-4xl">
+              {" "}
+              ({wishlistUpdated.length})
+            </span>
           </h1>
           {wishlistUpdated.length === 0 && (
-            <span className="relative flex mt-5 w-fit space-x-2 items-center border rounded-full cursor-pointer hover:bg-primary hover:border-primary border-black/10 py-4 pl-32 pr-4 overflow-hidden group">
+            <span className="relative flex w-fit space-x-2 items-center border rounded-full cursor-pointer hover:bg-primary hover:border-primary border-black/10 py-3 md:py-4 pl-32 pr-4 overflow-hidden group">
               <span className="absolute whitespace-nowrap left-4 w-full transition-all duration-300 ease-in-out transform group-hover:translate-y-[-100%] group-hover:opacity-0 opacity-100 translate-y-0">
                 View Products
               </span>
@@ -42,7 +45,7 @@ export default function Page() {
             </span>
           )}
         </div>
-        <div className="pt-10">
+        <div className="pt-4 md:pt-6 lg:pt-10">
           {wishlistUpdated && wishlistUpdated.length > 0 ? (
             wishlistUpdated.map((list: any) => {
               return (
@@ -63,7 +66,7 @@ export default function Page() {
               unoptimized
               alt={"Empty Wishlist"}
               src={"/assets/empty_wishlist.jpg"}
-              className="w-1/2 mx-auto object-contain rounded-md"
+              className="w-full lg:w-1/2 mx-auto object-contain rounded-md"
             />
           )}
         </div>
