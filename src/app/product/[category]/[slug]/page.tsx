15fca5c426dc9a3b41bf7c34c315c975c5b66d78
Rishabh gupta <guptarishabh792@gmail.com>
2024-11-28 16:46:40 +0530

    fix code

--- src/app/product/[category]/[slug]/page.tsx
+++ src/app/product/[category]/[slug]/page.tsx
@@ -1,8 +1,10 @@
 import { use } from "react";
+import Features from "@/components/common/Features";
 import Breadcrumbs from "../../components/Bradcrumbs";
 import ProductImage from "../../components/ProductImage";
 import ProductContent from "../../components/ProductContent";
 import ProductFeatures from "../../components/ProductFeatures";
+import RecommendedProducts from "@/components/common/RecommendedProducts";
 
 type ProductPageProps = {
   params: Promise<{
@@ -14,17 +16,23 @@ type ProductPageProps = {
 const ProductPage = ({ params }: ProductPageProps) => {
   const { category, slug } = use(params);
   const formattedSlug = slug.replace(/-/g, " ");
-
+  let products: any = [];
   return (
-    <div className="max-w-9xl mx-auto p-4 md:p-6 lg:p-10">
-      <Breadcrumbs category={category} formattedSlug={formattedSlug} />
-      <div className="mt-10 block lg:flex gap-12">
-        <ProductImage />
-        <ProductContent />
+    <>
+      <div className="max-w-9xl mx-auto p-4 md:p-6 lg:p-10">
+        <Breadcrumbs category={category} formattedSlug={formattedSlug} />
+        <div className="mt-10 block lg:flex gap-12">
+          <ProductImage />
+          <ProductContent />
+        </div>
+        <div className="bg-gray-300 h-[1px] my-[30px]" />
+        <ProductFeatures />
       </div>
-      <div className="bg-gray-300 h-[1px] my-[30px]" />
-      <ProductFeatures />
-    </div>
+      <div className="max-w-9xl mx-auto p-4 md:p-6 lg:p-10 !pt-0">
+        <RecommendedProducts products={products} />
+      </div>
+      <Features />
+    </>
   );
 };
 
