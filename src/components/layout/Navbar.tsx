1f3b098fa9059c52826fe78017d329e179a1e0cf
Rishabh gupta <guptarishabh792@gmail.com>
2024-11-28 18:46:21 +0530

    Update code

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,35 +1,46 @@
 import React from "react";
 import Link from "next/link";
+import Search from "./Search";
 import CartModal from "./Cart";
 import Wishlist from "./Wishlist";
 import ItemHover from "./ItemHover";
+import Categories from "./Categories";
+import BottomTabs from "./BottomTabs";
 import { links } from "@/data/country";
 import { Get } from "@/api/generalApi";
 import { FaRegUser } from "react-icons/fa";
 import MobileSidebar from "./MobileSidebar";
-import { IoMdSearch } from "react-icons/io";
 import { CiDiscount1 } from "react-icons/ci";
 import { LinkProps, NavbarProps } from "@/types/api";
-import Categories from "./Categories";
-import BottomTabs from "./BottomTabs";
+import Image from "next/image";
 
-const Navbar: React.FC<NavbarProps> = async ({ data }) => {
-  const resp = await Get("api/GetFullMenu?App=Worksafe");
-  const offerResponse = await Get("api/SpecialOffers");
-  let categories = resp.categories;
-  const offerExist = offerResponse.status;
+const Navbar: React.FC<NavbarProps> = async () => {
+  const [resp, logoResponse, offerResponse] = await Promise.allSettled([
+    Get("api/GetFullMenu?App=Worksafe"),
+    Get("api/WebLogo?App=Worksafe"),
+    Get("api/SpecialOffers"),
+  ]);
+
+  let categories = [];
+  let logoData = null;
+  let offerExist = false;
+
+  if (resp.status === "fulfilled") categories = resp?.value?.categories;
+  if (logoResponse.status === "fulfilled")
+    logoData = logoResponse?.value?.headerlogo;
+  if (offerResponse.status === "fulfilled")
+    offerExist = offerResponse?.value?.status;
+
   categories = !offerExist ? categories : categories.slice(0, 10);
+  console.log(logoData);
   return (
     <>
       <header className="bg-[#1C1C1C] sticky top-0 z-50 w-full text-white shadow-md">
         <div className="max-w-9xl mx-auto flex gap-5 items-center justify-between px-2 md:px-4 lg:px-6 py-2">
           {/* Logo */}
           <div className="w-[45%] flex justify-between items-center">
-            <Link
-              href="/"
-              className="bg-yellow-500 text-black text-center px-2 font-bold"
-            >
-              AXETOR <br /> WORKWEAR
+            <Link href="/">
+              <Image width={125} unoptimized height={100} src={logoData} alt="Logo" />
             </Link>
             <div className="space-x-5 lg:flex hidden items-center">
               {links.map((link: LinkProps) => {
@@ -42,34 +53,8 @@ const Navbar: React.FC<NavbarProps> = async ({ data }) => {
             </div>
           </div>
           <div className="w-[55%] flex gap-5 justify-end lg:justify-between items-center">
-            {/* Search */}
-            <div className="relative hidden w-full lg:block">
-              <input
-                type="text"
-                placeholder="What are you looking for?"
-                className="w-full px-4 py-3 text-gray-500 rounded-full outline-none"
-              />
-              <button className="absolute z-20 top-0 right-0 px-4 py-3 rounded-full">
-                <IoMdSearch size={25} className="text-black" />
-              </button>
-            </div>
-
-            {/* Right Side */}
+            <Search />
             <div className="flex items-center space-x-2 lg:space-x-4">
-              {/* <div className="border-r border-white/30 flex gap-2 pr-3">
-              <Link
-                href="#"
-                className="flex items-center text-white/50 font-semibold"
-              >
-                ENG
-              </Link>
-              <Link
-                href="#"
-                className="flex items-center text-white/50 font-semibold"
-              >
-                $USD
-              </Link>
-            </div> */}
               <Link
                 href="/my-account"
                 className="hover:text-yellow-500 hidden lg:block transition-all duration-100 ease-linear"
@@ -82,8 +67,6 @@ const Navbar: React.FC<NavbarProps> = async ({ data }) => {
             </div>
           </div>
         </div>
-
-        {/* Second Navbar */}
         <nav className="bg-white text-black">
           <div className="flex flex-wrap max-w-9xl mx-auto items-center justify-between px-4 md:px-6 py-3">
             <Categories categories={categories} />
@@ -104,3 +87,19 @@ const Navbar: React.FC<NavbarProps> = async ({ data }) => {
 };
 
 export default Navbar;
+{
+  /* <div className="border-r border-white/30 flex gap-2 pr-3">
+              <Link
+                href="#"
+                className="flex items-center text-white/50 font-semibold"
+              >
+                ENG
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center text-white/50 font-semibold"
+              >
+                $USD
+              </Link>
+            </div> */
+}
---
+++ src/components/layout/Search.tsx
@@ -0,0 +1,144 @@
+"use client";
+
+import Image from "next/image";
+import { Get } from "@/api/generalApi";
+import { toast } from "react-toastify";
+import { IoMdSearch } from "react-icons/io";
+import { bigShoulders } from "@/app/layout";
+import { useState, useEffect, useRef } from "react";
+
+const Search = () => {
+  const searchRef = useRef<HTMLDivElement | null>(null);
+
+  const [products, setProducts] = useState([]);
+  const [charIndex, setCharIndex] = useState(0);
+  const [openModal, setOpenModal] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  const [typingIndex, setTypingIndex] = useState(0);
+  const [placeholder, setPlaceholder] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+  const typingTextArray = [
+    "Find what inspires you!",
+    "Your perfect match is here!",
+    "Shop the hottest deals today!",
+    "Uncover the latest must-haves!",
+    "Transform your style with ease!",
+    "Explore exclusive collections now!",
+    "Search for something extraordinary!",
+    "Discover your next favorite product!",
+    "Start your search for amazing finds!",
+    "Browse top-rated items effortlessly!",
+  ];
+  const pauseTime = 1500;
+  const minSearchLength = 3;
+  const debounceDelay = 1000;
+  const typingSpeed = isDeleting ? 50 : 100;
+
+  const [debounceTimeout, setDebounceTimeout] = useState<NodeJS.Timeout | null>(
+    null
+  );
+
+  // Typewriter effect
+  useEffect(() => {
+    const currentString = typingTextArray[typingIndex];
+
+    if (!isDeleting && charIndex === currentString.length) {
+      setTimeout(() => setIsDeleting(true), pauseTime); // Pause before deleting
+    } else if (isDeleting && charIndex === 0) {
+      setIsDeleting(false);
+      setTypingIndex((prev) => (prev + 1) % typingTextArray.length); // Move to next string
+    } else {
+      const timeout = setTimeout(() => {
+        setCharIndex((prev) => prev + (isDeleting ? -1 : 1));
+        setPlaceholder(
+          currentString.slice(0, charIndex + (isDeleting ? -1 : 1))
+        );
+      }, typingSpeed);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [charIndex, isDeleting, typingIndex]);
+
+  useEffect(() => {
+    if (searchText.length < minSearchLength) return;
+    if (debounceTimeout) clearTimeout(debounceTimeout);
+
+    const timeout = setTimeout(() => {
+      callApi(searchText);
+    }, debounceDelay);
+
+    setDebounceTimeout(timeout);
+    return () => clearTimeout(timeout);
+  }, [searchText]);
+
+  const callApi = async (query: string) => {
+    const response = await Get("api/SearchProducts?search=" + query);
+    if (response.status) {
+      setOpenModal(true);
+      setProducts(response.product);
+    } else toast.info("No products found!");
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      if (searchRef.current && !searchRef.current.contains(e.target as Node)) {
+        setOpenModal(false);
+        setSearchText("");
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  return (
+    <div ref={searchRef} className="relative hidden w-full lg:block">
+      <input
+        type="text"
+        value={searchText}
+        placeholder={placeholder}
+        onChange={handleInputChange}
+        className="w-full px-4 py-3 text-gray-500 rounded-full outline-none"
+      />
+      <button className="absolute z-20 top-0 right-0 px-4 py-3 rounded-full">
+        <IoMdSearch size={25} className="text-black" />
+      </button>
+      {openModal && products.length > 0 && (
+        <div className="absolute w-full z-20 top-[58px] shadow-2xl bg-white right-0 text-gray-500 text-sm p-4 pb-2 rounded-lg overflow-auto max-h-64">
+          <ul className={`${bigShoulders.className} font-extrabold text-lg`}>
+            {products.map((product: any, index: number) => (
+              <li
+                key={index}
+                className="border-b cursor-pointer flex justify-between items-center py-2 text-gray-700"
+              >
+                <span className="hover:text-primary flex items-center gap-2">
+                  {/* <Image
+                    src={product?.ListingImage}
+                    alt={product?.Description}
+                    width={32}
+                    height={32}
+                    className="object-contain rounded-full"
+                  /> */}
+                  {product.Description ?? "Unnamed Product"}{" "}
+                </span>
+                <span>{product?.EndPrice}$</span>
+              </li>
+            ))}
+          </ul>
+          {openModal && products.length > 0 && (
+            <p className="text-center mt-2">
+              {`Showing ${products.length} results`}
+            </p>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Search;
