d21d565e6dfc17d0beffd052978151b7fec19daf
Rishabh gupta <guptarishabh792@gmail.com>
2024-12-06 17:56:55 +0530

    update code

--- src/app/product/components/QuantitySelector.tsx
+++ src/app/product/components/QuantitySelector.tsx
@@ -8,6 +8,7 @@ import SizeQuantities from "./SizeQuantities";
 import ProductFitting from "./ProductFitting";
 import AddToCartButton from "./AddToCartButton";
 import Logo from "@/components/customisation/Logo";
+import { toast } from "react-toastify";
 
 interface QuantitySelectorProps {
   product: Product;
@@ -23,10 +24,13 @@ const QuantitySelector = ({
     ProductActualPrice: product?.ProductActualPrice,
   });
   const [countItem, setCountItem] = useState(1);
+  const [filterProductSizes, setFilterProductSizes] = useState<any>(
+    product?.ProductSizes
+  );
   const [selectedFields, setSelectedFields] = useState({
     size: [],
-    color: {},
-    fitting: {},
+    color: { Colour_Sequence_No: "" },
+    fitting: { Fitting_Sequence_No: "" },
   });
 
   useEffect(() => {
@@ -51,9 +55,81 @@ const QuantitySelector = ({
     (fittings: any) => fittings?.Fitting?.trim() !== "NA"
   );
 
-  const filterProductSizes = product?.ProductSizes.filter(
-    (size: any) => size?.Size?.trim() !== "NA"
-  );
+  useEffect(() => {
+    const filterProductsForStock = (
+      products: any[],
+      filterProductSizes: any[]
+    ) => {
+      if (!Array.isArray(products) || !Array.isArray(filterProductSizes))
+        return [];
+
+      // Filter out invalid sizes
+      const validProductSizes: any[] =
+        filterProductSizes?.filter(
+          (size: any) => size?.Size?.trim() !== "NA"
+        ) || [];
+
+      // Create a stock map from products
+      const productStockMap = products.reduce((acc, product) => {
+        if (
+          product &&
+          typeof product.Size_Sequence_No !== "undefined" &&
+          typeof product.StockQty !== "undefined"
+        ) {
+          const keyParts = [
+            selectedFields.color ? product.Colour_Sequence_No : null,
+            product.Size_Sequence_No, // Always include size
+            selectedFields.fitting ? product.Fitting_Sequence_No : null,
+          ].filter((part) => part !== null); // Remove null values
+
+          const key = keyParts.join("_");
+          acc[key] = product.StockQty;
+        }
+        return acc;
+      }, {} as Record<string, number>);
+
+      // Map sizes to include stock quantity
+      const updatedSizes = validProductSizes.map((size) => {
+        const keyParts = [
+          selectedFields.color?.Colour_Sequence_No ?? null, // Include selected color if exists
+          size.Size_Sequence_No, // Always include size
+          selectedFields.fitting?.Fitting_Sequence_No ?? null, // Include selected fitting if exists
+        ].filter((part) => part !== null); // Remove null values
+
+        const key = keyParts.join("_");
+
+        const findStockQty = (keyPart: string) => {
+          const matchingKeys = Object.keys(productStockMap).filter((key) =>
+            key.includes(keyPart)
+          );
+          if (matchingKeys.length > 0) return productStockMap[matchingKeys[0]];
+          else return 0;
+        };
+        return {
+          ...size,
+          qty:
+            key && productStockMap[key]
+              ? productStockMap[key]
+              : findStockQty(key),
+        };
+      });
+      return updatedSizes;
+    };
+
+    if (
+      product?.ProductPricingByColourSizeFit &&
+      product?.ProductPricingByColourSizeFit.length > 0 &&
+      Array.isArray(filterProductSizes)
+    ) {
+      const filteredProductsForStock = filterProductsForStock(
+        product.ProductPricingByColourSizeFit,
+        product?.ProductSizes
+      );
+      setFilterProductSizes(filteredProductsForStock);
+    } else {
+      setFilterProductSizes([]);
+    }
+  }, [selectedFields, product]);
 
   useEffect(() => {
     const filterProducts = (products: any[], config: any) => {
@@ -87,6 +163,26 @@ const QuantitySelector = ({
     }
   }, [selectedFields, product]);
 
+  const fieldsCheck = () => {
+    if (
+      product?.ProductColour &&
+      product?.ProductColour.length > 0 &&
+      !selectedFields?.color?.Colour_Sequence_No
+    ) {
+      toast.info("Please select a color!");
+      return true;
+    }
+    if (
+      filterProductFittings &&
+      filterProductFittings.length > 0 &&
+      !selectedFields?.fitting?.Fitting_Sequence_No
+    ) {
+      toast.info("Please select a fitting!");
+      return true;
+    }
+    return false;
+  };
+
   return (
     <>
       {price?.ProductActualPrice && price?.ProductSellingPrice && (
@@ -104,18 +200,19 @@ const QuantitySelector = ({
           productColors={product?.ProductColour}
         />
       )}
-      {filterProductSizes.length > 0 && (
-        <SizeQuantities
-          sizes={filterProductSizes}
-          selectedFields={selectedFields}
-          setSelectedFields={setSelectedFields}
-        />
-      )}
       {filterProductFittings.length > 0 && (
         <ProductFitting
           selectedFields={selectedFields}
           setSelectedFields={setSelectedFields}
           productFittings={filterProductFittings}
+        />
+      )}
+      {filterProductSizes.length > 0 && (
+        <SizeQuantities
+          fieldsCheck={fieldsCheck}
+          sizes={filterProductSizes}
+          selectedFields={selectedFields}
+          setSelectedFields={setSelectedFields}
         />
       )}
       <div className="flex text-center py-5 gap-3">
--- src/app/product/components/SizeQuantities.tsx
+++ src/app/product/components/SizeQuantities.tsx
@@ -5,10 +5,12 @@ import { containerVariants, itemVariants } from "@/animation/framer";
 
 const SizeQuantities = ({
   sizes,
+  fieldsCheck,
   selectedFields,
   setSelectedFields,
 }: {
   sizes: any[];
+  fieldsCheck: any;
   selectedFields: any;
   setSelectedFields: any;
 }) => {
@@ -32,25 +34,33 @@ const SizeQuantities = ({
   }, [selectedFields]);
 
   const handleIncrement = (size: any) => {
-    setQuantities((prev) => {
-      const newQuantity = (prev[size.Size] || 0) + 1;
-      const updatedSize = { ...size, quantity: newQuantity };
+    if (fieldsCheck()) return;
 
-      setSelectedFields((prevFields: any) => {
-        const updatedFields = { ...prevFields };
-        if (!updatedFields.size) updatedFields.size = [];
-        const existingSizeIndex = updatedFields.size.findIndex(
-          (item: any) => item.Size === size.Size
-        );
-        if (existingSizeIndex === -1) updatedFields.size.push(updatedSize);
-        else updatedFields.size[existingSizeIndex] = updatedSize;
-        return updatedFields;
-      });
-      return { ...prev, [size.Size]: newQuantity };
+    setQuantities((prev) => {
+      const currentQuantity = prev[size.Size] || 0;
+      if (currentQuantity < 10) {
+        const newQuantity = currentQuantity + 1;
+        const updatedSize = { ...size, quantity: newQuantity };
+
+        setSelectedFields((prevFields: any) => {
+          const updatedFields = { ...prevFields };
+          if (!updatedFields.size) updatedFields.size = [];
+          const existingSizeIndex = updatedFields.size.findIndex(
+            (item: any) => item.Size === size.Size
+          );
+          if (existingSizeIndex === -1) updatedFields.size.push(updatedSize);
+          else updatedFields.size[existingSizeIndex] = updatedSize;
+          return updatedFields;
+        });
+        return { ...prev, [size.Size]: newQuantity };
+      }
+      return prev;
     });
   };
 
   const handleDecrement = (size: any) => {
+    if (fieldsCheck()) return;
+
     setQuantities((prev) => {
       const newQuantity = Math.max((prev[size.Size] || 0) - 1, 0);
       const updatedSize = { ...size, quantity: newQuantity };
@@ -77,7 +87,7 @@ const SizeQuantities = ({
     <div className="pb-2">
       <p className="mt-4 mb-2 font-semibold">Sizes</p>
       <motion.div
-        className="grid grid-cols-3 md:grid-cols-6 lg:grid-cols-5 gap-3 lg:gap-2"
+        className="grid grid-cols-4 md:grid-cols-7 lg:grid-cols-6 gap-3 lg:gap-2"
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true }}
@@ -88,22 +98,22 @@ const SizeQuantities = ({
           return (
             <motion.div
               key={size.Size}
-              className={`rounded-lg pt-2 p-[2px] text-center cursor-pointer ${
+              className={`rounded-lg pt-1 p-[2px] text-center cursor-pointer ${
                 isActive ? "bg-primary/70" : "bg-gray-200/80"
               }`}
               variants={itemVariants}
             >
               <div
-                className={`text-xl font-black ${
+                className={`font-black ${
                   isActive ? "text-black" : "text-gray-600"
                 } ${bigShoulders.className}`}
               >
                 {size.Size}
               </div>
-              <div className={`px-2 pt-px pb-1 mt-2 bg-white rounded-b-lg`}>
+              <div className={`px-1 pt-px pb-1 mt-1 bg-white rounded-b-lg`}>
                 <div className="flex items-center justify-between mt-2">
                   <button
-                    className={`w-6 h-6 flex justify-center items-center text-lg active:scale-[0.8] transition rounded ${
+                    className={`w-5 h-5 flex justify-center items-center active:scale-[0.8] transition rounded ${
                       isActive
                         ? "bg-primary/70 text-black hover:bg-primary scale-1-5"
                         : "bg-gray-300 text-white hover:bg-gray-400"
@@ -112,9 +122,9 @@ const SizeQuantities = ({
                   >
                     -
                   </button>
-                  <span>{quantities[size.Size] || 0}</span>
+                  <span className="text-sm">{quantities[size.Size] || 0}</span>
                   <button
-                    className={`w-6 h-6 flex justify-center items-center text-lg active:scale-[0.8] transition rounded ${
+                    className={`w-5 h-5 flex justify-center items-center active:scale-[0.8] transition rounded ${
                       isActive
                         ? "bg-primary/70 text-black hover:bg-primary scale-1-5"
                         : "bg-gray-300 text-white hover:bg-gray-400"
@@ -126,9 +136,7 @@ const SizeQuantities = ({
                 </div>
                 <div className="text-xs mt-2 pb-px">In Stock</div>
                 <div className="text-[10px] font-thin text-gray-600">
-                  {size.Size_Sequence_No > 5
-                    ? "32 Available"
-                    : "100+ Available"}
+                  {size.qty} Available
                 </div>
               </div>
             </motion.div>
